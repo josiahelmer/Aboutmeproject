f0bf37842009a87253ad22a4a3f51325b747c1ea
josiahelmer <moejoemann@gmail.com>
2015-10-12 07:24:21 -0600

    code for friends image

--- app/src/main/java/ctec/aboutmeproject/FriendsActivity.java
+++ app/src/main/java/ctec/aboutmeproject/FriendsActivity.java
@@ -8,11 +8,13 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ImageView;
 
 public class FriendsActivity extends AppCompatActivity
 {
     private Button  HomeButton;
     private RelativeLayout FriendsLayout;
+    private ImageView FriendsImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,7 @@ public class FriendsActivity extends AppCompatActivity
 
         HomeButton = (Button) findViewById(R.id.HomeFriendsButton);
         FriendsLayout =  (RelativeLayout) findViewById(R.id.FriendsLayout);
+        FriendsImage = (ImageView) findViewById(R.id.FriendsLayout);
 
         setupListeners();
 
